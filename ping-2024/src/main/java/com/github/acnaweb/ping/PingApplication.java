9a1e4f7a85d22ce51a00c2c5bbb982073239a6e3
Bernardo Assad <bernardoassad04@gmail.com>
2024-02-23 20:06:18 -0300

    Adicionando o projeto corretamente dentro do repositório

---
+++ ping-2024/src/main/java/com/github/acnaweb/ping/PingApplication.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.ping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingApplication.class, args);
+	}
+
+}
---
+++ ping-2024/src/test/java/com/github/acnaweb/ping/PingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
