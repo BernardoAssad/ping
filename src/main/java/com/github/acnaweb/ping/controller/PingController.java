75908bcaea302661b1b881f106bfc9308b2ad587
Bernardo Assad <bernardoassad04@gmail.com>
2024-02-23 22:50:26 -0300

    Setup

---
+++ src/main/java/com/github/acnaweb/ping/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.github.acnaweb.ping.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class PingController {
+	
+	@GetMapping("/ping")
+	public String ping() {
+		return "Pong";
+	}
+}
